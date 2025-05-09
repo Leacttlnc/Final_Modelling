202920fd8c4bc4214583dc36f16ba49f13c73eb0
leacouttolenc <leacouttolenc8@gmail.com>
2025-05-09 11:35:46 +0200

    change

--- Q2.py
+++ Q2.py
@@ -10,20 +10,20 @@ with open("company.csv", "w") as f:
 df = pd.read_csv("company.csv", index_col="Date")
 
 
-#1 How many rows does the dataframe have?
+# How many rows does the dataframe have?
 print("Number of rows:")
 print(len(df))
 
-#2 What are the column names?
+#What are the column names?
 print("\nColumn names:")
 print(df.columns)
 
-#3 What was the opening stock value on April 9th? We first need to make sure the index is parsed as dates
+# What was the opening stock value on April 9th? We first need to make sure the index is parsed as dates
 df.index = pd.to_datetime(df.index)
 print("\nOpening value on April 9th:")
 print(df.loc["2025-04-09", "Open"])
 
-#4 Plot one column
+#Plot one column
 plt.plot(df.index, df["Close"])
 plt.title("Ford Stock Close Price")
 plt.xlabel("Date")
